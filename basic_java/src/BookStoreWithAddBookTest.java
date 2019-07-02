ac41e30f40d9593523086797c4293c721362fc56
up1 <somkiat.p@gmail.com>
2019-07-02 15:28:44 +0700

    Manage basket and items

--- basic_java/src/BookStoreWithAddBookTest.java
+++ basic_java/src/BookStoreWithAddBookTest.java
@@ -25,5 +25,19 @@ public class BookStoreWithAddBookTest {
 		assertEquals(2, basket.getAmount());
 		assertEquals(1, basket.getLineItems());
 	}
+	
+	@Test
+	public void add_2_first_book_and_1_second_book_to_basket() {
+		BookStore bookStore = new BookStore();
+		Book firstBook1 = new Book("Harry Potter 1", 8, 10);
+		Book firstBook2 = new Book("Harry Potter 1", 8, 10);
+		Book secondBook1 = new Book("Harry Potter 2", 8, 10);
+		bookStore.pickItem(firstBook1);
+		bookStore.pickItem(firstBook2);
+		bookStore.pickItem(secondBook1);
+		Basket basket = bookStore.getCurrentBasket();
+		assertEquals(3, basket.getAmount());
+		assertEquals(2, basket.getLineItems());
+	}
 
 }
