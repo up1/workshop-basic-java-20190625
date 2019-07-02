16f2628e224d67ba2b88624aaa34ec3f1a0e0b80
up1 <somkiat.p@gmail.com>
2019-07-02 14:33:32 +0700

    Refactor

--- basic_java/src/Book.java
+++ basic_java/src/Book.java
@@ -1,3 +1,24 @@
 public class Book {
 
+	private String name;
+	private int price;
+	private int stock;
+	
+	public Book() {
+	}
+	public Book(String name, int price, int stock) {
+		this.name = name;
+		this.price = price;
+		this.stock = stock;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	
 }
--- basic_java/src/BookStoreTest.java
+++ basic_java/src/BookStoreTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.junit.Test;
 
 public class BookStoreTest {
-
+	
 	@Test
 	public void list_of_book_should_return_5_books() {
 		BookStore bookStore = new BookStore();
@@ -26,5 +26,25 @@ public class BookStoreTest {
 		List<Book> allBooks = bookStore.listAll();
 		assertEquals(5, allBooks.size());
 	}
+
+	private final class XXX implements BookDB {
+		@Override
+		public List<Book> getAll() {
+			List<Book> books = new ArrayList<>();
+			books.add(new Book("Harry Potter 1", 8, 10));
+			return books;
+		}
+	}
+	
+	@Test
+	public void detail_of_first_book() {
+		BookStore bookStore = new BookStore();
+		bookStore.setDb(new XXX());
+		List<Book> allBooks = bookStore.listAll();
+		assertEquals("Harry Potter 1", allBooks.get(0).getName());
+		assertEquals(8, allBooks.get(0).getPrice());
+		assertEquals(10, allBooks.get(0).getStock());
+	}
+
 
 }
