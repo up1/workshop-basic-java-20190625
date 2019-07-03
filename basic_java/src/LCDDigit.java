64e3b5ea912bab91229cdca1dfa3a7af9e6688d1
up1 <somkiat.p@gmail.com>
2019-07-03 16:42:34 +0700

    Working LCD Digit part 2

--- basic_java/src/LCDDigit.java
+++ basic_java/src/LCDDigit.java
@@ -1,42 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class LCDDigit {
-	
+
 	private int width = 1;
 	private int height = 1;
 
-	public LCDDigit() {}
-	
+	public LCDDigit() {
+	}
+
 	public LCDDigit(int width, int height) {
 		this.width = width;
 		this.height = height;
 	}
 
 	public String show(int number) {
-		String[] firstLine  = {"   ", " _ "};
-		String[] secondLine = {"  |", " _|"};
-		String[] thirdLine  = {"  |", "|_ "}; 
-		String[] result = {"", "", ""};
-		
+		String[] firstLine  = { "   ", " _ " };
+		String[] secondLine = { "  |", " _|" };
+		String[] thirdLine  = { "  |", "|_ " };
+		String[] result = { "", "", "" };
+
 		String input = String.valueOf(number);
 		int length = input.length();
-		
+		// Width
 		for (int i = 0; i < length; i++) {
-			result[0] +=  scaling(firstLine[input.charAt(i)-49]);
-			result[1] +=  scaling(secondLine[input.charAt(i)-49]);
-			result[2] +=  scaling(thirdLine[input.charAt(i)-49]);
+			result[0] += scaling(firstLine[input.charAt(i) - 49]);
+			result[1] += scaling(secondLine[input.charAt(i) - 49]);
+			result[2] += scaling(thirdLine[input.charAt(i) - 49]);
 		}
+
+		// Height
+		List<String> newResult = new ArrayList<>();
+		for (int line = 0; line < 3; line++) {
+			if(result[line].startsWith("|")) {
+				for(int i=0;i<height-1; i++) {
+					newResult.add(scaleWidth(result[line].length()));
+				}
+			}
+			if(result[line].endsWith("|")) {
+				for(int i=0;i<height-1; i++) {
+					newResult.add(scaleWidthLast(result[line].length()));
+				}
+			}
+			newResult.add(result[line]);
+		}
+
+		String finalResult = "";
+		for (String row : newResult) {
+			finalResult += row + "\n";
+		}
+
+		return finalResult;
+	}
+
+	private String scaleWidth(int length) {
+		String result = "|";
+		for(int i=0; i<length-1; i++) {
+			result += " ";
+		}
+		return result;
+	}
 	
-		return  result[0] + "\n" +
-				result[1] + "\n" +
-				result[2] + "\n";
+	private String scaleWidthLast(int length) {
+		String result = "";
+		for(int i=0; i<length-1; i++) {
+			result += " ";
+		}
+		return result + "|";
 	}
 
 	private String scaling(String input) {
 		String newInputWithScaled = "";
-		for(int i=0; i<width; i++) {
+		for (int i = 0; i < width; i++) {
 			newInputWithScaled += "_";
 		}
-		input = input.replace("_", newInputWithScaled );
+		input = input.replace("_", newInputWithScaled);
 		return input;
 	}
 
--- basic_java/src/LCDDigitWithScaleTest.java
+++ basic_java/src/LCDDigitWithScaleTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class LCDDigitWithScaleTest {
 
 	@Test
-	public void test() {
+	public void test2() {
 		String expectedResult = ""
 				+ " ___ \n"
 				+ "    |\n"
@@ -16,5 +16,58 @@ public class LCDDigitWithScaleTest {
 		LCDDigit lcd = new LCDDigit(3,2); 
 		assertEquals(expectedResult, lcd.show(2));
 	}
+	
+	@Test
+	public void test2_4() {
+		String expectedResult = ""
+				+ " ____ \n"
+				+ "     |\n"
+				+ " ____|\n"
+				+ "|     \n"
+				+ "|____ \n";
+		
+		LCDDigit lcd = new LCDDigit(4,2); 
+		assertEquals(expectedResult, lcd.show(2));
+	}
+	
+	@Test
+	public void test1() {
+		String expectedResult = ""
+				+ "   \n"
+				+ "  |\n"
+				+ "  |\n"
+				+ "  |\n"
+				+ "  |\n";
+		
+		LCDDigit lcd = new LCDDigit(3,2); 
+		assertEquals(expectedResult, lcd.show(1));
+	}
+	@Test
+	public void test1_3() {
+		String expectedResult = ""
+				+ "   \n"
+				+ "  |\n"
+				+ "  |\n"
+				+ "  |\n"
+				+ "  |\n"
+				+ "  |\n"
+				+ "  |\n";
+		
+		LCDDigit lcd = new LCDDigit(3,3); 
+		assertEquals(expectedResult, lcd.show(1));
+	}
+	
+//	@Test
+//	public void test12() {
+//		String expectedResult = ""
+//				+ "    ___ \n"
+//				+ "  |    |\n"
+//				+ "  | ___|\n"
+//				+ "  ||    \n"
+//				+ "  ||___ \n";
+//		
+//		LCDDigit lcd = new LCDDigit(3,2); 
+//		assertEquals(expectedResult, lcd.show(12));
+//	}
 
 }
