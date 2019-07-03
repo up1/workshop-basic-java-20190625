9a8bd763de5e54186d5155b9d167b0fd45bfda9b
up1 <somkiat.p@gmail.com>
2019-07-03 15:02:03 +0700

    Workshop LCD Digit part 1

--- basic_java/src/LCDDigit.java
+++ basic_java/src/LCDDigit.java
@@ -1,11 +1,24 @@
 
 public class LCDDigit {
 
-	public String show(int i) {
-		return ""
-				+ " _ \n"
-				+ " _|\n"
-				+ "|_ \n";
+	public String show(int number) {
+		String[] firstLine  = {"   ", " _ "};
+		String[] secondLine = {"  |", " _|"};
+		String[] thirdLine  = {"  |", "|_ "}; 
+		String[] result = {"", "", ""};
+		
+		String input = String.valueOf(number);
+		int length = input.length();
+		
+		for (int i = 0; i < length; i++) {
+			result[0] +=  firstLine[input.charAt(i)-49];
+			result[1] +=  secondLine[input.charAt(i)-49];
+			result[2] +=  thirdLine[input.charAt(i)-49];
+		}
+	
+		return  result[0] + "\n" +
+				result[1] + "\n" +
+				result[2] + "\n";
 	}
 
 }
--- basic_java/src/LCDDigitTest.java
+++ basic_java/src/LCDDigitTest.java
@@ -3,6 +3,28 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LCDDigitTest {
+	
+	@Test
+	public void input_1() {
+		String expectedResult = ""
+				+ "   \n"
+				+ "  |\n"
+				+ "  |\n";
+		
+		LCDDigit lcd = new LCDDigit(); 
+		assertEquals(expectedResult, lcd.show(1));
+	}
+	
+	@Test
+	public void input_12() {
+		String expectedResult = ""
+				+ "    _ \n"
+				+ "  | _|\n"
+				+ "  ||_ \n";
+		
+		LCDDigit lcd = new LCDDigit(); 
+		assertEquals(expectedResult, lcd.show(12));
+	}
 
 	@Test
 	public void input_2() {
@@ -11,8 +33,20 @@ public class LCDDigitTest {
 				+ " _|\n"
 				+ "|_ \n";
 		
-		LCDDigit lcd = new LCDDigit();
+		LCDDigit lcd = new LCDDigit(); 
 		assertEquals(expectedResult, lcd.show(2));
 	}
+	
+	@Test
+	public void input_22() {
+		String expectedResult = ""
+				+ " _  _ \n"
+				+ " _| _|\n"
+				+ "|_ |_ \n";
+		
+		LCDDigit lcd = new LCDDigit();
+		assertEquals(expectedResult, lcd.show(22));
+	}
+
 
 }
